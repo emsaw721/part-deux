95b352899354610d392f37fcae927a1e59dd0f9b
Emily Hill <emhillmed2021@gmail.com>
2023-01-03 12:15:14 -0600

    trying to add a sticky feature to the homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -10,10 +10,14 @@ export default function Home() {
   <style>
 @import url('https://fonts.googleapis.com/css2?family=Lato:wght@300&family=League+Script&display=swap');
 </style>
+<div className='hello'>
       <div className='name-plate'>
       <h1>hello</h1>
       </div>
+      <div className='homeimg'>
       <img src={headshot} />
+      </div>
+      </div>
       <p className='intro'> My name is <span style={{color: '#415A77'}}> Emily Hill </span> and I am a full stack developer based in <span style={{color: '#415A77'}}>Houston, TX</span></p>
       <div className='background'>
         <div className='bgimgwrapper'>
@@ -23,10 +27,14 @@ export default function Home() {
       <p>I grew up in Houston, but spent 10 years in New York City, where I attended Fordham University. There, earned my Bachelor of Arts in English and completed a Post-Baccelaureate Pre-Medical program.</p>
       </div>
       </div>
-      {/* <div className='accomplishments'>
-      <img src="https://via.placeholder.com/250" />
+      <div className='accomplishments'>
+      <div className='accpara'>
         <p>While in New York, I persued many service opportunities. I served as an Emergency Medical Technician, a tutor, and, during the COVID-19 pandemic, as a Nurse's Aid at St. John's Riverside Hospital.</p>
-      </div> */}
+        </div>
+        <div className='accimgwrapper'>
+      <img src="https://via.placeholder.com/250" />
+      </div>
+      </div> 
     </div>
   );
 }

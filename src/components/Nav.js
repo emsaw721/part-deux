9eb59de583fb783bf8a2d6d5f8b71ab886070ef7
Emily Hill <emhillmed2021@gmail.com>
2023-01-05 17:11:36 -0600

    fixed some hover issues in nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,40 +8,21 @@ function Nav({ currentPage, handlePageChange }) {
 @import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Lato:wght@300&family=League+Script&family=Playfair+Display:wght@900&display=swap');
 </style>
          <div className='resumebtn'>
-       <button><img src={download} />Download Full Resume</button>
+       <button><img src={download} />Download Resume</button>
       </div>
       <div>
     <ul className="nav">
       <li className="nav-item hover-2">
-        <a
-          href="#home"
-          onClick={() => handlePageChange('Home')}
-          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-        >
-          Home
-        </a>
+        <a href="#home" onClick={() => handlePageChange('Home')} className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}>Home</a>
       </li>
       <li className="nav-item hover-2">
         <a href="#projects" onClick={() => handlePageChange('Projects')} className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'} >Projects</a>
       </li>
       <li className="nav-item hover-2">
-        <a
-          href="#resume"
-          onClick={() => handlePageChange('Resume')}
-          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-        >
-          Resume
-        </a>
+        <a href="#resume" onClick={() => handlePageChange('Resume')} className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>Resume</a>
       </li>
       <li className="nav-item hover-2">
-        <a
-          href="#contact"
-
-          onClick={() => handlePageChange('Contact')}
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact
-        </a>
+        <a href="#contact" onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>Contact</a>
       </li>
     </ul>
     </div>

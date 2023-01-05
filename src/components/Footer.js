a206d9204f9cc03fa4c0eafe787ff0a51b93adf7
Emily Hill <emhillmed2021@gmail.com>
2023-01-05 16:50:34 -0600

    flipped image to tilt the same way as hello

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import './Footer.css';
+import github from './pages/githubicon.png';
+import linkedin from './pages/linkedinicon.png'; 
+import instagram from './pages/instagramicon.png';
 
 function Footer() {
     return (
-            <div className='footer'>
-                <style>
+<div className='footer'>
+ <style>
 @import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Lato:wght@300&family=League+Script&family=Playfair+Display:wght@900&display=swap');
 </style>
-                
-                        <a href="https://github.com/emsaw721">GitHub</a>
-                   
-                        <a href="#linkedin">LinkedIn</a>
-                 
-                        <a href="#instagram">Instagram</a>
-              
-            </div>
-    )
+<a href="https://github.com/emsaw721"><img src={github}/></a>
+<a href="https://linkedin.com/in/emily-hill-95a545241"><img src={linkedin}/></a>
+<a href="#instagram"><img src={instagram}/></a>
+</div>
+)
 }
 
 export default Footer; 
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,7 +4,6 @@ import gradcap from './gradcap.png';
 import code from './codeblocks.png'; 
 import volicon from './volunteericon.png'; 
 import workicon from './workicon.png'; 
-import download from './download.png';
 
 
 function Resume() {
@@ -27,19 +26,22 @@ function Resume() {
         <h1>At <span>・</span> A <span>・</span> Glance</h1>
         <div className='glance'>
         <img src={gradcap} /> B.A. & B.S. Fordham University <br/>
-        <img src={code} />
-           {/* <h3>Front End</h3>
+        <img src={code} />Coding Bootcamp at Rice University <span className='shortlistlink'> ✳︎see <a href='#theshortlist'>The Short List</a>✳︎</span><br/>
+        <img src={volicon} /> EMT, Tutor, & Theatrical Outreach  <br/>
+        <img src={workicon} /> Medical Examiner, Literary Intern, & Nurse's Aid<br/>
+        </div>
+        <div id='theshortlist'>
+        <h2>The Short List</h2>
+        <h3>Front End</h3>
             <ul>
                 <li>{getFEList}</li>
             </ul>
             <h3>Back End</h3>
             <ul>
                 <li>{getBEList}</li>
-            </ul> */} <br/>
-        <img src={volicon} /> EMT, Tutor, & Theatrical Outreach  <br/>
-        <img src={workicon} /> Medical Examiner, Literary Intern, & Nurse's Aid<br/>
-        </div>
+            </ul> 
         </div> 
+        </div>
     )
 }
 

ad4f2d35f1ffd7f90e1d7b7ccdf9c07fd79a4096
Emily Hill <emhillmed2021@gmail.com>
2023-03-30 12:10:28 -0500

    added coming soon heading to blog page, blog page next

--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import './Blog.css'; 
 
 function Blog() {
 return (
-    <section className='blog'>
+<section className='blog'>
         <style>
-@import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Lato:wght@300&family=League+Script&family=Playfair+Display:wght@900&display=swap');
-</style>
-    </section>
+            @import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Lato:wght@300&family=League+Script&family=Playfair+Display:wght@900&display=swap');
+        </style>
+<div className='blogcontent'>  
+<h1>Coming Soon!</h1>
+</div>
+</section>
 )
 }
 
